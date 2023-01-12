a05f03961576d56efe2bb101f9ad5b4570df6212
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2023-01-12 15:48:36 +0600

    Create customize_default_theme.el

---
+++ customize_default_theme.el
@@ -0,0 +1,5 @@
+(load-theme 'tango-dark t)
+(set-foreground-color "ivory")
+(set-background-color "darkblue")
+(set-face-attribute 'region nil :background "#666" :foreground "#ffffff")	; Change the HIGHLIGHT COLOR for SELECTED TEXT
+(set-face-attribute 'highlight nil :foreground 'unspecified)
